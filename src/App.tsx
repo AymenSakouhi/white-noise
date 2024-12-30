25c8310c390e1e8510c00293adc81ed803fe068d
Aymen Sakouhi <aymenmarketer@gmail.com>
2024-12-30 21:31:47 +0100

    adding auth with a custom auth provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
 import './App.css'
 import Layout from './components/Layout'
-import WhiteNoisePlayer from './components/WhiteNoisePlayer'
-import { soundsAssets } from './helpers/utils'
+import WhiteNoisePlayer from '@/components/WhiteNoisePlayer'
+import { soundsAssets } from '@/helpers/utils'
 import { BsSoundwave } from 'react-icons/bs'
-import { auth } from './firebase'
-import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { useAuth } from '@/components/AuthContext'
+import GoogleSignIn from '@/components/GoogleSignIn'
 
-export const GoogleSignIn = () => {
-  const handleGoogleSignIn = async () => {
-    const provider = new GoogleAuthProvider()
-    try {
-      const result = await signInWithPopup(auth, provider)
-      const user = result.user
-      console.log('user info', user)
-    } catch (error) {
-      console.error('Error during google sign in', error)
-    }
-  }
-  return (
-    <div>
-      <button onClick={handleGoogleSignIn}>sign in with google</button>
-    </div>
-  )
-}
-
+// eslint-disable-next-line
 const whiteNoisesArr =
-  // eslint-disable-next-line
   Object.entries(soundsAssets)?.map(([path, module]) => {
     return {
       title: path?.split('/')?.pop()?.split('.')?.shift() || '',
@@ -35,22 +17,27 @@ const whiteNoisesArr =
   }) || []
 
 function App() {
+  const currentUser = useAuth()
+
   return (
     <>
-      <GoogleSignIn />
-      <Layout>
-        {whiteNoisesArr &&
-          whiteNoisesArr?.map((_) => (
-            <WhiteNoisePlayer
-              key={_.path}
-              {...{
-                title: _.title,
-                path: _.path,
-                Icon: _.Icon,
-              }}
-            />
-          ))}
-      </Layout>
+      {currentUser ? (
+        <Layout>
+          {whiteNoisesArr &&
+            whiteNoisesArr?.map((_) => (
+              <WhiteNoisePlayer
+                key={_.path}
+                {...{
+                  title: _.title,
+                  path: _.path,
+                  Icon: _.Icon,
+                }}
+              />
+            ))}
+        </Layout>
+      ) : (
+        <GoogleSignIn />
+      )}
     </>
   )
 }
