cd90529e6db6500e281f024297efeed38cd96a3d
Aymen Sakouhi <aymenmarketer@gmail.com>
2024-12-31 11:02:21 +0100

    making the reset button and rewriting the component for timer

--- src/components/Pomodoro.tsx
+++ src/components/Pomodoro.tsx
@@ -7,19 +7,29 @@ enum TIMERANGES {
   HOUR = 60 * 60 * 1000,
 }
 const Pomodoro = () => {
+  const [mode, setMode] = useState(TIMERANGES.QUARTER)
   const [timer, setTimer] = useState(TIMERANGES.QUARTER)
-  const [timerActive, setTimerActive] = useState(false)
+  const [isActive, setIsActive] = useState(false)
 
   useEffect(() => {
-    if (timerActive) {
-      setTimeout(() => {
+    let timerInterval: NodeJS.Timeout
+    if (isActive && timer > 0)
+      timerInterval = setInterval(() => {
         setTimer((prev) => prev - 1000)
       }, 1000)
-    }
-  }, [timerActive, timer])
 
-  const handleTimer = (mode) => {
-    setTimerActive(!timerActive)
+    return () => {
+      clearInterval(timerInterval)
+    }
+  }, [isActive, timer])
+
+  const handleTimer = () => {
+    setIsActive(!isActive)
+  }
+
+  const handleReset = () => {
+    setIsActive(false)
+    setTimer(mode)
   }
   return (
     <>
@@ -38,7 +48,11 @@ const Pomodoro = () => {
             handleTimer('HALF')
           }}
         >
-          {!timerActive ? 'Start' : 'Pause'}
+          {!isActive ? 'Start' : 'Pause'}
+        </button>
+
+        <button disabled={!isActive} onClick={handleReset}>
+          Reset
         </button>
       </div>
     </>
