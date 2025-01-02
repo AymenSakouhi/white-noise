8686fd128a78764e5be01529155d66249b562803
Aymen Sakouhi <aymenmarketer@gmail.com>
2025-01-02 18:30:21 +0100

    fixing out the header

--- src/components/reusable/Header.tsx
+++ src/components/reusable/Header.tsx
@@ -7,10 +7,8 @@ const Header = () => {
 
   return (
     <>
-      <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen p-2">
-        <a href="/" className="text-2xl  font-semibold flex items-center">
-          Study with me!
-        </a>
+      <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen p-2 bg-teal-800 border-b-2 border-teal-700">
+        <a className="text-2xl  font-mono flex items-center">Study with me!!</a>
         <div className="flex items-center lg:order-2">
           <a
             onClick={() => {
@@ -22,13 +20,12 @@ const Header = () => {
           </a>
         </div>
         <div
-          className="xs:hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+          className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
           id="mobile-menu-2"
         >
           <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
             <li>
               <a
-                href="#"
                 className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
                 aria-current="page"
               >
@@ -36,10 +33,7 @@ const Header = () => {
               </a>
             </li>
             <li>
-              <a
-                href="#"
-                className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700"
-              >
+              <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
                 Contact
               </a>
             </li>
