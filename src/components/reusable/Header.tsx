fa4c43d542077c7d63f37bee1c81265a447ea161
Aymen Sakouhi <aymenmarketer@gmail.com>
2025-01-28 00:21:51 +0100

    finishing a well-not-done hearder --

--- src/components/reusable/Header.tsx
+++ src/components/reusable/Header.tsx
@@ -6,41 +6,37 @@ const Header = () => {
   const currentUser = useAuth()
 
   return (
-    <>
-      <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen p-2 bg-teal-800 border-b-2 border-teal-700">
-        <a className="text-2xl  font-mono flex items-center">Study with me!!</a>
-        <div className="flex items-center lg:order-2">
-          <a
-            onClick={() => {
-              signOut(auth)
-            }}
-            className="cursor-pointer text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
-          >
-            {currentUser ? 'Log Out' : 'Log In'}
+    <header className="bg-gray-900 text-gray-100">
+      <div className="container mx-auto flex items-center justify-between p-4">
+        {/* logo */}
+        <div className="text-lg font-bold">
+          <a href="#" className="hover:text-gray-300">
+            mylogo
           </a>
         </div>
-        <div
-          className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
-          id="mobile-menu-2"
-        >
-          <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
-            <li>
-              <a
-                className="block py-2 pr-4 pl-3 text-white rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white"
-                aria-current="page"
-              >
-                Home
-              </a>
-            </li>
-            <li>
-              <a className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
       </div>
-    </>
+
+      {/* Navigation */}
+      <nav className="hidden md:flex space-x-6">
+        <a href="#">Home</a>
+        <a href="#">About</a>
+        <a href="#">Services</a>
+        <a href="#">Contact</a>
+      </nav>
+
+      <div>
+        {/* Logout session */}
+
+        <a
+          onClick={() => {
+            signOut(auth)
+          }}
+          className="cursor-pointer text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+        >
+          {currentUser ? 'Log Out' : 'Log In'}
+        </a>
+      </div>
+    </header>
   )
 }
 
