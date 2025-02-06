90c3be8d567517013221c7832b1b98fdab7d3fac
Aymen Sakouhi <aymenmarketer@gmail.com>
2025-02-06 23:34:20 +0100

    we added formik correctly with the correct layout for the contact page

--- src/routes/contact.tsx
+++ src/routes/contact.tsx
@@ -1,5 +1,6 @@
+import Layout from '@/components/reusable/Layout'
 import { createFileRoute } from '@tanstack/react-router'
-import { Field, Formik, FormikHelpers } from 'formik'
+import { Field, Form, Formik, FormikHelpers } from 'formik'
 
 export const Route = createFileRoute('/contact')({
   component: Contact,
@@ -11,27 +12,29 @@ type Values = {
 
 function Contact() {
   return (
-    <div className="p-2">
-      <Formik
-        initialValues={{
-          firstName: '',
-        }}
-        onSubmit={(
-          values: Values,
-          { setSubmitting }: FormikHelpers<Values>,
-        ) => {
-          setTimeout(() => {
-            alert(values)
-            setSubmitting(false)
-          }, 1000)
-        }}
-      >
-        <form>
-          <label htmlFor="firstName">First Name</label>
-          <Field id="firstName" name="firstName" placeholder="John" />
-          <button type="submit">send</button>
-        </form>
-      </Formik>
-    </div>
+    <Layout>
+      <div className="p-2">
+        <Formik
+          initialValues={{
+            firstName: '',
+          }}
+          onSubmit={(
+            values: Values,
+            { setSubmitting }: FormikHelpers<Values>,
+          ) => {
+            setTimeout(() => {
+              alert(values.firstName)
+              setSubmitting(false)
+            }, 1000)
+          }}
+        >
+          <Form>
+            <label htmlFor="firstName">First Name</label>
+            <Field id="firstName" name="firstName" placeholder="John" />
+            <button type="submit">send</button>
+          </Form>
+        </Formik>
+      </div>
+    </Layout>
   )
 }
