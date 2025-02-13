d3ee39ceee1a0bc56579fdbeb7e9db50a9a050ea
Aymen Sakouhi <aymenmarketer@gmail.com>
2025-02-13 23:36:29 +0100

    fix on the timer

--- src/components/reusable/Timer/index.tsx
+++ src/components/reusable/Timer/index.tsx
@@ -9,12 +9,13 @@ type TimerProps = {
 const BACKGROUNDIMAGE = 'https://pagedone.io/asset/uploads/1710565658.jpg'
 
 const Timer: React.FC<TimerProps> = ({ timer }) => {
-  const minutes = timer.split(':')[0]
-  const seconds = timer.split(':')[1]
+  // shout out to tony for the destructuring hint
+  const [minutes, seconds] = timer.split(':')
 
   return (
     <div
-      className={`bg-[url('${BACKGROUNDIMAGE}')] h-96 rounded-2xl flex gap-9 flex-col items-center justify-center bg-cover bg-center`}
+      className={`h-96 rounded-2xl flex gap-9 flex-col items-center justify-center bg-cover bg-center`}
+      style={{ backgroundImage: `url(${BACKGROUNDIMAGE})` }}
     >
       <div className="flex items-start justify-center w-full gap-1.5 count-down-main">
         <Slot
