635be9dc3cc42e0d860acc5d8e64d4c2e1567009
Aymen Sakouhi <aymenmarketer@gmail.com>
2025-02-17 01:40:02 +0100

    updating todos

--- src/components/AuthContext.tsx
+++ src/components/AuthContext.tsx
@@ -1,3 +1,5 @@
+//TODO DELETE THIS ASAAAAAAAAP
+
 import {
   useContext,
   useState,
--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -4,6 +4,7 @@ import { getAnalytics } from 'firebase/analytics'
 import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
+//TODO delete this ASAAAAAAAAAAAAP
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
