02ea2a96f23e6033fb33378d6a096084e5cbeae3
Aymen Sakouhi <aymenmarketer@gmail.com>
2025-03-07 02:43:26 +0100

    adding middleware for dataverification

--- server/src/routes/route.ts
+++ server/src/routes/route.ts
@@ -11,11 +11,14 @@ import {
   addTodo,
 } from '@src/controllers/controllers'
 
+import { validateData } from '@src/middlewares/middlewares'
+import { userLoginSchema, userRegisterSchema } from '@src/schemas/schemas'
+
 export const routes = Router()
 // sanity check api
 routes.get('/status', checkSanity)
-routes.post('/register', userRegister)
-routes.post('/login', userLogin)
+routes.post('/register', validateData(userRegisterSchema), userRegister)
+routes.post('/login', validateData(userLoginSchema), userLogin)
 routes.post('/logout', checkBlacklist, userLogout)
 // protected profile route
 routes.post('/profile', userAuthenticateMiddleWare, userAuthenticate)
