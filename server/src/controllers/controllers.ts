8ad22be2b18cbf464c3979552f8d41190929f1c7
Aymen Sakouhi <aymenmarketer@gmail.com>
2025-03-07 01:44:28 +0100

    removed query and disconnect

--- server/src/controllers/controllers.ts
+++ server/src/controllers/controllers.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express'
-import prisma, { queryAndDisconnect } from '@src/db/init'
+import prisma from '@src/db/init'
 import bcrypt from 'bcryptjs'
 import passport from 'passport'
 import jwt from 'jsonwebtoken'
@@ -10,66 +10,63 @@ export const checkSanity = (req: Request, res: Response) => {
   })
 }
 
-export const userRegister = (req: Request, res: Response) => {
-  queryAndDisconnect(async () => {
-    const { name, email, password } = req.body
-    console.log(name, email, password)
-    try {
-      const existingUser = await prisma.user.findUnique({
-        where: {
-          email,
-        },
-        select: { id: true, email: true, password: true },
-      })
-      if (existingUser) {
-        return res.status(400).json({ message: 'User already exists!!' })
-      }
-      const hashedPassword = await bcrypt.hash(password, 10)
-      await prisma.user.create({
-        data: {
-          name,
-          password: hashedPassword,
-          email,
-        },
-      })
-      res.status(201).json({ message: 'User registered successfully' })
-    } catch (error) {
-      res.status(500).json({
-        message: 'USERREGISTER: Server error',
-        error,
-      })
+export const userRegister = async (req: Request, res: Response) => {
+  const { name, email, password } = req.body
+  try {
+    const existingUser = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+      select: { id: true, email: true, password: true },
+    })
+    if (existingUser) {
+      res.status(400).json({ message: 'User already exists!!' })
     }
-  })
+    const hashedPassword = await bcrypt.hash(password, 10)
+    await prisma.user.create({
+      data: {
+        name,
+        password: hashedPassword,
+        email,
+      },
+    })
+    res.status(201).json({ message: 'User registered successfully' })
+  } catch (error) {
+    res.status(500).json({
+      message: 'USERREGISTER: Server error',
+      error,
+    })
+  }
 }
 
-export const userLogin = (req: Request, res: Response) => {
-  queryAndDisconnect(async () => {
-    const { email, password } = req.body
-    try {
-      const user = await prisma.user.findUnique({
-        where: {
-          email,
-        },
-        select: { id: true, email: true, password: true },
-      })
-      if (!user)
-        return res.status(400).json({ message: 'User not found, you dummy!!' })
-      const isMatch = await bcrypt.compare(password, user.password)
-      if (!isMatch) {
-        return res.status(400).json({ message: 'Invalid credentials' })
-      }
-      const payload = { id: user.id, email: user.email }
-      const token = jwt.sign(payload, process.env.JWT_TOKEN as string, {
-        expiresIn: '4h',
-      })
-      res.status(201).json({ token })
-    } catch (error) {
-      res.status(500).json({
-        message: 'USERLOGIN: Server error',
-        error,
-      })
+export const userLogin = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+      select: { id: true, email: true, password: true },
+    })
+    if (!user) {
+      res.status(400).json({ message: 'User not found, you dummy!!' })
+      return
     }
-  })
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+      res.status(400).json({ message: 'Invalid credentials' })
+    }
+    const payload = { id: user.id, email: user.email }
+    const token = jwt.sign(payload, process.env.JWT_TOKEN as string, {
+      expiresIn: '4h',
+    })
+    res.status(201).json({ token })
+  } catch (error) {
+    res.status(500).json({
+      message: 'USERLOGIN: Server error',
+      error,
+    })
+  }
 }
 // black list to store tokens
 const tokenBlacklist = new Set<string>()
