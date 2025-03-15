ff5f098293bdfaab5a99f173096a18f086cb0e4f
Aymen Sakouhi <aymenmarketer@gmail.com>
2025-03-15 03:55:56 +0100

    add new way to upload vite config

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+export interface ImportMetaEnv {
+  readonly VITE_BASEURL: string
+  // more env variables...
+}
+
+export interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
