6a4b856f4a8d1a87a73cb58943019aeb54f14d08
Aymen Sakouhi <aymenmarketer@gmail.com>
2025-03-17 01:46:53 +0100

    adding a new contant for vite baseurl

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const BASEURL = import.meta.env.VITE_BASEURL
   ? import.meta.env.VITE_BASEURL
-  : 'http://bkwks04cg8cscg4cgkwk4co4.193.203.190.82.sslip.io'
+  : 'http://b0c0cc8wgcokwcgc44okgo4k.193.203.190.82.sslip.io'
